892072f6646ded16400954327cdb3cdfb98bb88b
omerdr7 <omerfarukdere32@gmail.com>
2022-12-30 14:36:54 +0300

    web sayfasına gitme kodu
    web sayfasına gitme kodu

---
+++ istisna işlemler şablonlar/istisna işlemler şablonlar.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int x;
+	std::cout << "Gitmek Istediginiz Siteyi Seciniz(1->Youtube/2->Udemy):";
+	std::cin >> x;
+	if (x == 1) {
+		system("start https://www.youtube.com/");
+	}
+	else if(x == 2) {
+		system("start https://www.udemy.com/");
+	}
+else {
+	std::cout << "Yanlis Numara Girdiniz!";
+}
+}
